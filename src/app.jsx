13a50f37c4356f95136c49544b76e3216f665fdd
Alastair <alastairzotos@gmailcom>
2020-10-26 15:47:27 +0000

    added counter

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
 function MyPage() {
@@ -8,6 +9,7 @@ function MyPage() {
             <p>Please have a look around</p>
  
             <AboutMe name="Joe" />
+            <Counter />
         </div>
     )
 }
@@ -17,6 +19,18 @@ function AboutMe(props) {
         <div>
             <h3>About me</h3>
             <p>Hello my name is {props.name}</p>
+        </div>
+    )
+}
+
+function Counter() {
+    var [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+
+            <p>Count: {count}</p>
         </div>
     )
 }
